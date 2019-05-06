f38251a4cba149ef877c500abd68c56316c729a2
cloudhao1999 <cyh199910@outlook.com>
2019-05-06 21:43:39 +0800

    小改动2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var list=["1","2","3","4"];
+  var list=["1","2","3","4","5"];
   res.render('index',{list:list});
 });
 
